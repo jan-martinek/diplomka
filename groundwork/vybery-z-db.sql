adbd6bd2eda88b2924280c7d55cd529a2768368e
Jan Martinek <jan.martinek@proofreason.com>
2013-05-14 16:04:34 +0200

    Hodnocení, podklady

--- groundwork/vybery-z-db.sql
+++ groundwork/vybery-z-db.sql
@@ -14,7 +14,28 @@ SELECT projects.name, count(*) AS notEl FROM projects, eligibilities AS el
 WHERE projects.id = el.projectID AND el.notEligible = 1 AND el.phase = 'secondRating'
 GROUP BY projects.id;
 
-# wtf, skutečně?
+# std. error hodnocení projektů
 
-SELECT roundID, STD(rating), ratings.phase, STD(firstRatingIneligibility), STD(secondRatingIneligibility) FROM ratings LEFT JOIN projects ON ratings.projectID = projects.id
-GROUP BY projects.roundID, ratings.phase
+SELECT projects.name, projects.id, firstRatingIneligibility, secondRatingIneligibility, STD(r1.rating), STD(r2.rating)
+FROM projects, ratings as r1, ratings as r2
+WHERE (r1.projectID = projects.id AND r1.phase = 'firstRating')
+AND (r2.projectID = projects.id AND r2.phase = 'secondRating')
+GROUP BY projects.id
+
+# std. error v jednotlivých kolech
+
+SELECT roundID, STD(r1.rating), STD(r2.rating)
+FROM projects, ratings as r1, ratings as r2
+WHERE (r1.projectID = projects.id AND r1.phase = 'firstRating')
+AND (r2.projectID = projects.id AND r2.phase = 'secondRating')
+GROUP BY projects.roundID
+
+# std. error v jednotlivých kolech bez projektů, které dostaly nedoporučení
+
+SELECT roundID, STD(r1.rating), STD(r2.rating)
+FROM projects, ratings as r1, ratings as r2
+WHERE (r1.projectID = projects.id AND r1.phase = 'firstRating')
+AND (r2.projectID = projects.id AND r2.phase = 'secondRating')
+AND projects.secondRatingIneligibility < 2
+GROUP BY projects.roundID
+
